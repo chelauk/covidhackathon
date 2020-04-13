50cef0dda2affa21334c50d25cf54520541842ae
suzannejin <suzanne.jin@crg.eu>
2020-04-13 10:16:50 +0200

    Modified scrape_software_versions.py

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -9,6 +9,9 @@ regexes = {
     'Nextflow': ['v_nextflow.txt', r"(\S+)"],
     'FastQC': ['v_fastqc.txt', r"FastQC v(\S+)"],
     'MultiQC': ['v_multiqc.txt', r"multiqc, version (\S+)"],
+    'STAR' : ['v_star.txt', r"(\S+)"],
+    'HISAT2' : ['v_hisat2.txt', r"version (\S+)"],
+    'stringtie' : ['v_stringtie.txt', r"(\S+)"]
 }
 results = OrderedDict()
 results['nf-core/covidhackathon'] = '<span style="color:#999999;\">N/A</span>'
